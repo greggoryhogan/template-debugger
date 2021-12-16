9ef8641018781ed5af68cb36dbfcc62fea9bc356
Gregg Hogan <21093023+ostensiblycool@users.noreply.github.com>
2021-12-16 10:39:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Template Debugger
+
+This plugin queries all the page templates in your active theme and sees if they are being used or not. To use the plugin, add the shortcode [template_debugger] to any post or page and view it on the front end.
---
+++ template-debugger.php
@@ -0,0 +1,68 @@
+<?php
+/*
+Plugin Name: Template Debugger
+Description: Query all page templates and see if they are being used or not
+Author: Greggory Hogan 
+Version: 1.0.0
+Author URI: https://mynameisgregg.com
+*/
+
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
+
+class TemplateDebugger {
+   
+	/**
+	 * Setup the shortcode's properties
+	 *
+	 * @param array  $atts
+	 * @param string $content
+	 */
+	public function __construct() {
+        $this->init_shortcodes();
+	}
+
+    /**
+     * Add actions for course record
+     * 
+     * @return void
+     */
+    private function init_shortcodes() {
+        add_shortcode('template_debugger',array($this,'get_template_data'));
+    }
+
+     /**
+     * Pull metadata from course record
+     */
+    public function get_template_data() {
+        $template_data = '';
+        $templates = wp_get_theme()->get_page_templates();
+        foreach ( $templates as $file => $name ) {
+            $q = new WP_Query( array(
+                'post_type' => 'page',
+                'posts_per_page' => -1,
+                'meta_query' => array( array(
+                    'key' => '_wp_page_template',
+                    'value' => $file
+                ) )
+            ) );
+            $page_count = sizeof( $q->posts );
+            if ( $page_count > 0 ) {
+                $template_data .= '<p style="margin-top: 15px;"><strong>' . $file . ': <em>' . sizeof( $q->posts ) . '</em> pages are using this template:</strong><br>';        
+                foreach ( $q->posts as $p ) {
+                    $template_data .= '<a href="' . get_permalink( $p, false ) . '" target="_blank" style="display: block; margin-left: 15px;">' . $p->post_title . '</a>';
+                }
+                $template_data .= "</p>";
+            } else {
+                //$template_data .= $file.'<br>';
+                $template_data .= '<p style="color:red">' . $file . ': <strong>0</strong> pages are using this template, you should be able to safely delete it from your theme.</p>';
+            }
+            /*foreach ( $q->posts as $p ) {
+                $report[$file][$p->ID] = $p->post_title;
+            }*/
+        }   
+        // Reset our postdata:
+        wp_reset_postdata();
+        return $template_data;
+    }
+}
+new TemplateDebugger();
